aa7780b665b0070255edf4d191459549a650fc4a
Donald Doricent <ddoricent97@gmail.com>
2022-12-04 11:09:46 -0500

    Variable and console log exercises

--- exercise01/javascript.js
+++ exercise01/javascript.js
@@ -1,9 +1,20 @@
-console.log("This is here through external link JS.")
-let admin = "John"
-let name = "John"
+// console.log("This is here through external link JS.")
+// let admin = "John"
+// let name = "John"
 // alert(admin) //
 
-let myPlanet = "Earth"
-let currentVisitor = "YOU!"
+// let myPlanet = "Earth"
+// let currentVisitor = "YOU!"
 
-alert(currentVisitor)
+// alert(currentVisitor)
+
+let a = 10
+console.log(a)
+console.log(9 * a)
+let b = 7 * a
+console.log(b)
+
+const max = 57
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage)
