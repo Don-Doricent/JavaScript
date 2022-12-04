853a6eea3fdd95553f8bbc41929142bfcf17f973
Donald Doricent <ddoricent97@gmail.com>
2022-12-04 10:48:38 -0500

    finished up with variables on exercise1.

--- exercise01/javascript.js
+++ exercise01/javascript.js
@@ -1,4 +1,9 @@
 console.log("This is here through external link JS.")
 let admin = "John"
 let name = "John"
-alert(admin)
+// alert(admin) //
+
+let myPlanet = "Earth"
+let currentVisitor = "YOU!"
+
+alert(currentVisitor)
