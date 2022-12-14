881624b78b20ddef8987ae719af89c1b0faefff7
Donald Doricent <ddoricent97@gmail.com>
2022-12-13 20:28:49 -0500

    Worked through the FizzBuzz exercise.

---
+++ FizzBuzz/main.js
@@ -0,0 +1,20 @@
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+ // for (let i = 1; i <= answer; i++) { //
+  //  console.log(i); //
+// } //
+
+// This is a for loop. We declare a variable (i) and assign it 1: the initial value of the variable (i) in our loop. The second clause, i <= answer is our condition. We want to loop until i is greater than answer. The third clause, i++, tells our loop to increment i by 1 every iteration. As a result, if the user inputs 10, this loop would print numbers 1 - 10 to the console. //
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 === 0) {
+    console.log("Fizz");
+}   else if (i % 5 === 0){
+    console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
