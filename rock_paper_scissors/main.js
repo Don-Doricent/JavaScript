42a238bc47e79c71171a74cae7c5dcd98d2285e5
Donald Doricent <ddoricent97@gmail.com>
2022-12-20 17:51:35 -0500

    modified.

---
+++ rock_paper_scissors/main.js
@@ -0,0 +1,7 @@
+// Start with getting a computer to choose out of rock, paper, or scissors randomly
+
+function getComputerchoice(){
+    if (getComputerchoice = ('rock')){
+        return(rock)
+    }
+}
