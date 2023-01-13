8bed5a0a3f1341c42be862ea416898e2ee9b60cd
Donald Doricent <ddoricent97@gmail.com>
2023-01-13 08:03:40 -0500

    testing out my dom minipulation.

---
+++ DOM-Practice/main.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('#container');
+
+const pText = document.createElement('p');
+pText.classList.add('pText');
+pText.textContent = 'Hey! Im Red!';
+container.appendChild(pText)
