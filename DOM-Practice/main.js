dcd08b1d1e347345a96e3a336edf5285cd6f44b7
Donald Doricent <ddoricent97@gmail.com>
2023-01-13 08:07:42 -0500

    WE ARE MANIPULATINGgit add main.js

--- DOM-Practice/main.js
+++ DOM-Practice/main.js
@@ -3,4 +3,9 @@ const container = document.querySelector('#container');
 const pText = document.createElement('p');
 pText.classList.add('pText');
 pText.textContent = 'Hey! Im Red!';
-container.appendChild(pText)
+container.appendChild(pText);
+
+const hThree = document.createElement('h3');
+hThree.classList.add('hThree');
+hThree.textContent = "i'm a blue h3!";
+container.appendChild(hThree);
