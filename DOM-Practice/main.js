11addbf7318721bb6007bc9b26d141b2ea51f76a
Donald Doricent <ddoricent97@gmail.com>
2023-01-16 07:08:08 -0500

    Trying to figure out how to append an h1 to a div on javascript.

--- DOM-Practice/main.js
+++ DOM-Practice/main.js
@@ -9,3 +9,11 @@ const hThree = document.createElement('h3');
 hThree.classList.add('hThree');
 hThree.textContent = "i'm a blue h3!";
 container.appendChild(hThree);
+
+const div = document.createElement('div');
+div.appendChild('h1');
+div.appendChild('p');
+h1.textContent = "i'm in a div";
+p.textContent = "ME TOO!";
+container.appendChild(h1);
+container.appendChild(p);
